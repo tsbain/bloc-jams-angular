1c772b35a4340ad9e86365ef0411d7097897b093
Tim Bain <timsbain@gmail.com>
2015-12-10 14:45:19 -0800

    assignment 5 completed

--- app/scripts/controllers/AlbumCtrl.js
+++ app/scripts/controllers/AlbumCtrl.js
@@ -1,19 +1,7 @@
 (function() {
-  function AlbumCtrl() {
-    this.albumData = {
-    	name: 'The Colors',
-    	artist: 'Pablo Picasso',
-    	label: 'Cubism',
-    	year: '1881',
-    	albumArtUrl: 'assets/images/album_covers/02.png',
-    	songs: [
-    		{ name: 'Blue', length: '161.71', audioUrl: 'assets/music/blue' },
-    		{ name:	'Green', length: '103.96', audioUrl: 'assets/music/green' },
-    		{ name:	'Red', length: '268.45', audioUrl: 'assets/music/red' },
-    		{ name:	'Pink', length: '153.14', audioUrl: 'assets/music/pink' },
-    		{ name:	'Magenta', length: '374.22', audioUrl: 'assets/music/magenta' }
-    	]
-    }
+  function AlbumCtrl($scope) {
+    // this.albumData = albumPicasso;
+    $scope.albumData = albumPicasso;
   };
 
   angular
