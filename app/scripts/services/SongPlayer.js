a592395ddf1d368aae7bf98cd13d8473690a4892
Tim Bain <timsbain@gmail.com>
2015-12-13 15:58:27 -0800

    checkpoint 7 completed

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,7 +3,15 @@
     var SongPlayer = {};
 
     var currentSong = null;
+
+    // @desc Buzz object audio fule
+    // @type {Object}
+
     var currentBuzzObject = null;
+
+    // @function setSong
+    // @desc Stops currently playing song and loads new audio file as currentBuzzObject
+    // @param {Object} song
 
     var setSong = function(song) {
       if (currentBuzzObject) {
