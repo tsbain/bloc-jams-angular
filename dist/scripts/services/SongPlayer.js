3cf3c5d54ab9c5bb09db87b99037a11be38d7180
Tim Bain <timsbain@gmail.com>
2015-12-14 17:40:48 -0800

    checkpoint 8 progress

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -2,8 +2,6 @@
   function SongPlayer() {
     var SongPlayer = {};
 
-    var currentSong = null;
-
     // @desc Buzz object audio file
     // @type {Object}
 
@@ -16,8 +14,8 @@
     var setSong = function(song) {
       if (currentBuzzObject) {
         currentBuzzObject.stop();
-        currentSong.playing = null;
-      } else if (currentSong === song) {
+        SongPlayer.currentSong.playing = null;
+      } else if (SongPlayer.currentSong === song) {
         if (currentBuzzObject.isPaused()) {
           currentBuzzObject.play();
         }
@@ -28,24 +26,40 @@
         preload: true
       });
 
-      currentSong = song;
+      SongPlayer.currentSong = song;
     };
+
+    // @function play
+    // @desc Play current or new song
+    // @param {Object} song
 
     var playSong = function(song) {
       currentBuzzObject.play();
       song.playing = true;
     };
 
+    SongPlayer.currentSong = null;
+
     SongPlayer.play = function(song) {
-      if (currentSong !== song) {
+      song = song || SongPlayer.currentSong;
+      if (SongPlayer.currentSong !== song) {
         setSong(song);
         // currentBuzzObject.play();
         // song.playing = true;
         playSong(song);
+      } else if (SongPlayer.currentSong === song) {
+        if (currentBuzzObject.isPaused()) {
+          playSong(song);
+        }
       }
     };
 
+    // @function pause
+    // @desc Pause current song
+    // @param {Object} song
+
     SongPlayer.pause = function(song) {
+      song = song || SongPlayer.currentSong;
       currentBuzzObject.pause();
       song.playing = false;
     };
