7d77191d1585de5ea19995e63e34117604d273d1
Tim Bain <timsbain@gmail.com>
2016-01-05 17:45:29 -0800

    checkpoint 10 completed

--- app/scripts/directives/seekBar.js
+++ app/scripts/directives/seekBar.js
@@ -13,12 +13,22 @@
       templateUrl: '/templates/directives/seek_bar.html',
       replace: true,
       restrict: 'E',
-      scope: {},
-      link: function(scope, element, attribute) {
+      scope: {
+        onChange: '&'
+      },
+      link: function(scope, element, attributes) {
         scope.value = 0;
         scope.max = 100;
 
         var seekBar = $(element);
+
+        attributes.$observe('value', function(newValue) {
+          scope.value = newValue;
+        });
+
+        attributes.$observe('max', function(newValue) {
+          scope.max = newValue;
+        });
 
         var percentString = function() {
           var value = scope.value;
@@ -32,12 +42,13 @@
         };
 
         scope.thumbStyle = function() {
-          return { left: percentString() }
+          return {left: percentString()}
         }
 
         scope.onClickSeekBar = function(event) {
           var percent = calculatePercent(seekBar, event);
           scope.value = percent * scope.max;
+          notifyOnChange(scope.value);
         };
 
         scope.trackThumb = function() {
@@ -45,8 +56,15 @@
             var percent = calculatePercent(seekBar, event);
             scope.$apply(function() {
               scope.value = percent * scope.max;
+              notifyOnChange(scope.value);
             });
           });
+
+        var notifyOnChange = function(newValue) {
+          if (typeof scope.onChange === 'function') {
+            scope.onChange({value: newValue});
+          }
+        };
 
           $document.bind('mouseup.thumb', function() {
             $document.unbind('mousemove.thumb');
--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,5 +1,5 @@
 (function() {
-  function SongPlayer(Fixtures) {
+  function SongPlayer($rootScope, Fixtures) {
     var SongPlayer = {};
 
     // @desc Inject Fixtures service, access and store song and album info
@@ -21,15 +21,21 @@
         // currentBuzzObject.stop();
         // SongPlayer.currentSong.playing = null;
         stopSong(song);
-      } else if (SongPlayer.currentSong === song) {
-        if (currentBuzzObject.isPaused()) {
-          currentBuzzObject.play();
-        }
+      // } else if (SongPlayer.currentSong === song) {
+      //   if (currentBuzzObject.isPaused()) {
+      //     currentBuzzObject.play();
+      //   }
       }
 
       currentBuzzObject = new buzz.sound(song.audioUrl, {
         formats: ['mp3'],
         preload: true
+      });
+
+      currentBuzzObject.bind('timeupdate', function() {
+        $rootScope.$apply(function() {
+          SongPlayer.currentTime = currentBuzzObject.getTime();
+        });
       });
 
       SongPlayer.currentSong = song;
@@ -61,6 +67,11 @@
     };
 
     SongPlayer.currentSong = null;
+
+    // @desc Current playback time (in seconds) of currently playing song
+    // @type {Number}
+
+    SongPlayer.currentTime = null;
 
     SongPlayer.play = function(song) {
       song = song || SongPlayer.currentSong;
@@ -120,10 +131,20 @@
       }
     };
 
+    // @function setCurrentTime
+    // @desc Set current time (in seconds) of currently playing song
+    // @param {Number} time
+
+    SongPlayer.setCurrentTime = function(time) {
+      if (currentBuzzObject) {
+        currentBuzzObject.setTime(time);
+      }
+    };
+
     return SongPlayer;
   }
 
   angular
     .module('blocJams')
-    .factory('SongPlayer', SongPlayer);
+    .factory('SongPlayer', ['$rootScope', 'Fixtures', SongPlayer]);
 })();
